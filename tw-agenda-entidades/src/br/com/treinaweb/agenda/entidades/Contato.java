cdc081703d72666824fb483d3402b820933984f6
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-19 15:51:52 -0300

    Aula 4.7: definindo o projeto de entidades da agenda

---
+++ tw-agenda-entidades/src/br/com/treinaweb/agenda/entidades/Contato.java
@@ -0,0 +1,33 @@
+package br.com.treinaweb.agenda.entidades;
+
+public class Contato {
+
+	private String nome;
+	private int idade;
+	private String telefone;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
