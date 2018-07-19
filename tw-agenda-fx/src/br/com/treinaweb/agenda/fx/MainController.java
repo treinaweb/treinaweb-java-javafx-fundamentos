3b5718cf4b9c96296a24535dd0e544b18d8d9442
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-19 16:57:02 -0300

    Aula 5.18: definindo interações entre componentes do JavaFX para a implementação da exclusão de contatos na agenda

--- tw-agenda-fx/src/br/com/treinaweb/agenda/fx/MainController.java
+++ tw-agenda-fx/src/br/com/treinaweb/agenda/fx/MainController.java
@@ -2,6 +2,7 @@ package br.com.treinaweb.agenda.fx;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import br.com.treinaweb.agenda.entidades.Contato;
@@ -11,13 +12,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class MainController implements Initializable {
-	
+
 	@FXML
 	private TableView<Contato> tabelaContatos;
 	@FXML
@@ -36,7 +40,7 @@ public class MainController implements Initializable {
 	private Button botaoSalvar;
 	@FXML
 	private Button botaoCancelar;
-	
+
 	private Boolean ehInserir;
 	private Contato contatoSelecionado;
 
@@ -44,28 +48,31 @@ public class MainController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		this.tabelaContatos.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 		habilitarEdicaoAgenda(false);
-//		this.tabelaContatos.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Contato>() {
-//
-//			@Override
-//			public void changed(ObservableValue<? extends Contato> observable, Contato oldValue, Contato newValue) {
-//				if (newValue != null) {
-//					txfNome.setText(newValue.getNome());
-//					txfIdade.setText(String.valueOf(newValue.getIdade()));
-//					txfTelefone.setText(newValue.getTelefone());
-//				}
-//			}
-//		});
-		this.tabelaContatos.getSelectionModel().selectedItemProperty().addListener((observador, contatoAntigo, contatoNovo) -> {
-			if (contatoNovo != null) {
-				txfNome.setText(contatoNovo.getNome());
-				txfIdade.setText(String.valueOf(contatoNovo.getIdade()));
-				txfTelefone.setText(contatoNovo.getTelefone());
-				this.contatoSelecionado = contatoNovo;
-			}
-		});
+		// this.tabelaContatos.getSelectionModel().selectedItemProperty().addListener(new
+		// ChangeListener<Contato>() {
+		//
+		// @Override
+		// public void changed(ObservableValue<? extends Contato> observable, Contato
+		// oldValue, Contato newValue) {
+		// if (newValue != null) {
+		// txfNome.setText(newValue.getNome());
+		// txfIdade.setText(String.valueOf(newValue.getIdade()));
+		// txfTelefone.setText(newValue.getTelefone());
+		// }
+		// }
+		// });
+		this.tabelaContatos.getSelectionModel().selectedItemProperty()
+				.addListener((observador, contatoAntigo, contatoNovo) -> {
+					if (contatoNovo != null) {
+						txfNome.setText(contatoNovo.getNome());
+						txfIdade.setText(String.valueOf(contatoNovo.getIdade()));
+						txfTelefone.setText(contatoNovo.getTelefone());
+						this.contatoSelecionado = contatoNovo;
+					}
+				});
 		carregarTabelaContatos();
 	}
-	
+
 	public void botaoInserir_Action() {
 		this.ehInserir = true;
 		this.txfNome.setText("");
@@ -73,7 +80,7 @@ public class MainController implements Initializable {
 		this.txfTelefone.setText("");
 		habilitarEdicaoAgenda(true);
 	}
-	
+
 	public void botaoAlterar_Action() {
 		habilitarEdicaoAgenda(true);
 		this.ehInserir = false;
@@ -81,12 +88,26 @@ public class MainController implements Initializable {
 		this.txfIdade.setText(Integer.toString(this.contatoSelecionado.getIdade()));
 		this.txfTelefone.setText(this.contatoSelecionado.getTelefone());
 	}
-	
+
+	public void botaoExcluir_Action() {
+		Alert confirmacao = new Alert(AlertType.CONFIRMATION);
+		confirmacao.setTitle("Confirmação");
+		confirmacao.setHeaderText("Confirmação da exclusão do contato");
+		confirmacao.setContentText("Tem certeza de que deseja excluir este contato?");
+		Optional<ButtonType> resultadoConfirmacao = confirmacao.showAndWait();
+		if (resultadoConfirmacao.isPresent() && resultadoConfirmacao.get() == ButtonType.OK) {
+			AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
+			repositorioContato.excluir(this.contatoSelecionado);
+			carregarTabelaContatos();
+			this.tabelaContatos.getSelectionModel().selectFirst();
+		}
+	}
+
 	public void botaoCancelar_Action() {
 		habilitarEdicaoAgenda(false);
 		this.tabelaContatos.getSelectionModel().selectFirst();
 	}
-	
+
 	public void botaoSalvar_Action() {
 		AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
 		Contato contato = new Contato();
@@ -102,7 +123,7 @@ public class MainController implements Initializable {
 		carregarTabelaContatos();
 		this.tabelaContatos.getSelectionModel().selectFirst();
 	}
-	
+
 	private void carregarTabelaContatos() {
 		AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
 		List<Contato> contatos = repositorioContato.selecionar();
@@ -116,7 +137,7 @@ public class MainController implements Initializable {
 		ObservableList<Contato> contatosObservableList = FXCollections.observableArrayList(contatos);
 		this.tabelaContatos.getItems().setAll(contatosObservableList);
 	}
-	
+
 	private void habilitarEdicaoAgenda(Boolean edicaoEstaHabilitada) {
 		this.txfNome.setDisable(!edicaoEstaHabilitada);
 		this.txfIdade.setDisable(!edicaoEstaHabilitada);
@@ -128,5 +149,5 @@ public class MainController implements Initializable {
 		this.botaoExcluir.setDisable(edicaoEstaHabilitada);
 		this.tabelaContatos.setDisable(edicaoEstaHabilitada);
 	}
-	
+
 }
