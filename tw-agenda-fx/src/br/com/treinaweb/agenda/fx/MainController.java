ed789bc75dbfdd5e2c50ecac6e56f9426a5d70a8
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-19 16:34:19 -0300

    Aula 5.15: utilizando o TableView para exibição de informações

--- tw-agenda-fx/src/br/com/treinaweb/agenda/fx/MainController.java
+++ tw-agenda-fx/src/br/com/treinaweb/agenda/fx/MainController.java
@@ -1,12 +1,22 @@
 package br.com.treinaweb.agenda.fx;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import br.com.treinaweb.agenda.entidades.Contato;
+import br.com.treinaweb.agenda.repositorios.impl.ContatoRepositorio;
+import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class MainController {
+public class MainController implements Initializable {
 	
 	@FXML
 	private TableView<Contato> tabelaContatos;
@@ -26,5 +36,25 @@ public class MainController {
 	private Button botaoSalvar;
 	@FXML
 	private Button botaoCancelar;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.tabelaContatos.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+		carregarTabelaContatos();
+	}
+	
+	private void carregarTabelaContatos() {
+		AgendaRepositorio<Contato> repositorioContato = new ContatoRepositorio();
+		List<Contato> contatos = repositorioContato.selecionar();
+		if (contatos.isEmpty()) {
+			Contato contato = new Contato();
+			contato.setNome("TreinaWeb");
+			contato.setIdade(12);
+			contato.setTelefone("123456");
+			contatos.add(contato);
+		}
+		ObservableList<Contato> contatosObservableList = FXCollections.observableArrayList(contatos);
+		this.tabelaContatos.getItems().setAll(contatosObservableList);
+	}
 	
 }
