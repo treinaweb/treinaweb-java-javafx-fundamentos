78459aacfddb1a61e183fcf0ef06c69dee4a18a3
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-19 15:41:24 -0300

    Aula 2.2: definições básicas em um projeto JavaFX

--- tw-agenda-fx/src/br/com/treinaweb/agenda/fx/Main.java
+++ tw-agenda-fx/src/br/com/treinaweb/agenda/fx/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Minha agenda");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
