ac5d9b5266a7d0aa02476fd7cafbfb2404813c92
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-19 16:05:23 -0300

    Aula 4.10: implementando interfaces genéricas

---
+++ tw-agenda-repositorios/src/br/com/treinaweb/agenda/repositorios/impl/ContatoRepositorio.java
@@ -0,0 +1,34 @@
+package br.com.treinaweb.agenda.repositorios.impl;
+
+import java.util.List;
+
+import br.com.treinaweb.agenda.entidades.Contato;
+import br.com.treinaweb.agenda.repositorios.interfaces.AgendaRepositorio;
+
+public class ContatoRepositorio implements AgendaRepositorio<Contato> {
+
+	@Override
+	public List<Contato> selecionar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserir(Contato entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Contato entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Contato entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
