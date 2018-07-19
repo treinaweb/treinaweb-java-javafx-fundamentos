4ee7dd5c16a64e93d5e32ee7c87513037d6acee3
Cleber Campomori <cleber.campomori@gmail.com>
2018-07-19 16:26:01 -0300

    Aula 5.14: ligando uma interface definida em FXML com código Java

--- tw-agenda-fx/src/br/com/treinaweb/agenda/fx/MainController.java
+++ tw-agenda-fx/src/br/com/treinaweb/agenda/fx/MainController.java
@@ -1,5 +1,30 @@
 package br.com.treinaweb.agenda.fx;
 
+import br.com.treinaweb.agenda.entidades.Contato;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainController {
+	
+	@FXML
+	private TableView<Contato> tabelaContatos;
+	@FXML
+	private Button botaoInserir;
+	@FXML
+	private Button botaoAlterar;
+	@FXML
+	private Button botaoExcluir;
+	@FXML
+	private TextField txfNome;
+	@FXML
+	private TextField txfIdade;
+	@FXML
+	private TextField txfTelefone;
+	@FXML
+	private Button botaoSalvar;
+	@FXML
+	private Button botaoCancelar;
 	
 }
